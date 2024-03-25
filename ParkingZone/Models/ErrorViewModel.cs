6aa83e488171d2dc4e1a265f2c5a192c7f6acca9
iamurtazo <murtazo207@inha.edu>
2024-03-25 23:21:31 +0500

    Add project files.

---
+++ ParkingZone/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ParkingZone.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
