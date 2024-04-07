b1d9ba7464d1ce2caa3f6f7c5a02143d52cf0abb
iamurtazo <murtazo207@inha.edu>
2024-04-07 18:31:56 +0500

    Repository Design Pattern Added

---
+++ ParkingZoneApp/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace ParkingZoneApp.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> GetAll();
+        T GetById(int? Id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ ParkingZoneApp/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ParkingZoneApp.Data;
+
+namespace ParkingZoneApp.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public void Delete(T entity)
+        {
+            _dbSet.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T GetById(int? Id)
+        {
+            return _dbSet.Find(Id);
+        }
+
+        public void Insert(T entity)
+        {
+            _dbSet.Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Update(entity);    
+            _context.SaveChanges();
+        }
+    }
+}
